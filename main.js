45aff22bc116fe62ad4ed554c59e4e3b66e77303
anle9650 <andy.le@ucdenver.edu>
2021-05-02 19:32:51 -0600

    update

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const mongoose = require("mongoose"),
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  process.env.MONGO_URI || "mongodb://localhost:27017/confetti_cuisine",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/confetti_cuisine",
   {useNewUrlParser: true}
 );
 
